bce49bb90af593de4ec20d8eb2a1a6fe0b7d1865
Rishi9926 <sonirishe9926@gmail.com>
2023-12-19 15:13:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var t1 = gsap.timeline()
+t1.from(".nav  img, h5, .l3, i",{
+    y:-100,
+    daley:0.5,
+    duration:1.5,
+    stagger:0.1,
+    
+})
+gsap.from(".m1 h1",{
+   x:-600,
+   daley:3,
+   duration:2,
+   stagger:1
+})
